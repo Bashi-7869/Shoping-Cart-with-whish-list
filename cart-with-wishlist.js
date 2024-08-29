4c24cb32294b6c436b8aea780f195ae5e85ba297
Bashi-7869 <121685218+Bashi-7869@users.noreply.github.com>
2024-08-29 12:51:13 +0530

    formating and update

--- cart-with-wishlist.js
+++ cart-with-wishlist.js
@@ -1,25 +1,23 @@
-let iconCart = document.querySelector(".icon-wishlish");
-let closebtn = document.querySelector(".tab-item .close");
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContent = document.querySelectorAll("[content]");
+button = document.querySelectorAll("[content] .close");
 let body = document.querySelector("body");
 
-iconCart.addEventListener("click", () => {
-  body.classList.toggle("active-tab");
-});
-closebtn.addEventListener("click", () => {
-  body.classList.toggle("active-tab");
-});
-
-const tabs = document.querySelectorAll("[data-target]"),
-  tabContent = document.querySelectorAll("[content]"),
-  button = document.querySelector("[content] .close");
-
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
+tabs.forEach((tab) =>{
+  tab.addEventListener("click",()=>{
+    body.classList.toggle("active-tab");
     const target = document.querySelector(tab.dataset.target);
-    // console.log(target);
+    // console.log(target)
     tabContent.forEach((content) => {
       content.classList.remove("active-tab");
     });
     target.classList.add("active-tab");
-  });
+    
+  });  
 });
+
+button.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    body.classList.toggle("active-tab");
+  });
+ });
